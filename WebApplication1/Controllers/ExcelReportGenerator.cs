8970b0e8bcef0ac4bb6f0fa2bc3cef4cb8fee08d
http://blog.followyourshot.com/ <admin@blog.followyourshot.com>
2017-04-24 23:17:07 +0200

    Add project files.

---
+++ WebApplication1/Controllers/ExcelReportGenerator.cs
@@ -0,0 +1,70 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Op = DocumentFormat.OpenXml.CustomProperties;
+using Vt = DocumentFormat.OpenXml.VariantTypes;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class ExcelReportGenerator
+    {
+        internal void CreateExcelDocNew(MemoryStream mem)
+        {
+            using (SpreadsheetDocument document = SpreadsheetDocument.Create(mem, SpreadsheetDocumentType.Workbook, true))
+            {
+                var relationshipId = "rId1";
+
+                //build Workbook Part
+                var workbookPart = document.AddWorkbookPart();
+                var workbook = new Workbook();
+                var sheets = new Sheets();
+                var sheet1 = new Sheet() { Name = "Walter's Sheet", SheetId = 1, Id = relationshipId };
+                sheets.Append(sheet1);
+                workbook.Append(sheets);
+                workbookPart.Workbook = workbook;
+
+                //build Worksheet Part
+                var workSheetPart = workbookPart.AddNewPart<WorksheetPart>(relationshipId);
+                var workSheet = new Worksheet();
+                workSheet.Append(new SheetData());
+                workSheetPart.Worksheet = workSheet;
+
+                //add document properties
+                document.PackageProperties.Creator = "Ctrl_Alt_Defeat";
+                document.PackageProperties.ContentStatus = "Final";
+                document.PackageProperties.Created = DateTime.UtcNow;
+
+
+
+                CustomFilePropertiesPart customFilePropertiesPart1 = document.AddNewPart<CustomFilePropertiesPart>("rId4");
+                GenerateCustomFilePropertiesPart1Content(customFilePropertiesPart1);
+
+            }
+        }
+
+
+
+        private void GenerateCustomFilePropertiesPart1Content(CustomFilePropertiesPart customFilePropertiesPart1)
+        {
+            Op.Properties properties2 = new Op.Properties();
+            properties2.AddNamespaceDeclaration("vt", "http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes");
+
+            Op.CustomDocumentProperty customDocumentProperty1 = new Op.CustomDocumentProperty() { FormatId = "{D5CDD505-2E9C-101B-9397-08002B2CF9AE}", PropertyId = 2, Name = "_MarkAsFinal" };
+            Vt.VTBool vTBool1 = new Vt.VTBool();
+            vTBool1.Text = "true";
+
+            customDocumentProperty1.Append(vTBool1);
+
+            properties2.Append(customDocumentProperty1);
+
+            customFilePropertiesPart1.Properties = properties2;
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            var custId = 1234; //ToDo get from session when it is set
+
+            using (MemoryStream mem = new MemoryStream())
+            {
+                ExcelReportGenerator excel = new ExcelReportGenerator();
+
+                excel.CreateExcelDocNew(mem);
+
+                var reportName = string.Format("MyReport_{0}.xlsx", custId);
+                var returnFile = File(mem.ToArray(), System.Net.Mime.MediaTypeNames.Application.Octet, reportName);
+
+                return returnFile;
+            }
+
+        }
+    }
+}
