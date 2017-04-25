002a1252626c3bc6104c944d3cedfd1756797cda
http://blog.followyourshot.com/ <admin@blog.followyourshot.com>
2017-04-25 22:59:43 +0200

    This version makes the excel read only by using a protected sheet.

--- WebApplication1/Controllers/ExcelReportGenerator.cs
+++ WebApplication1/Controllers/ExcelReportGenerator.cs
@@ -1,14 +1,8 @@
 ﻿using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
-using Op = DocumentFormat.OpenXml.CustomProperties;
-using Vt = DocumentFormat.OpenXml.VariantTypes;
-
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Web;
 
 namespace WebApplication1.Controllers
 {
@@ -24,7 +18,7 @@ namespace WebApplication1.Controllers
                 var workbookPart = document.AddWorkbookPart();
                 var workbook = new Workbook();
                 var sheets = new Sheets();
-                var sheet1 = new Sheet() { Name = "Walter's Sheet", SheetId = 1, Id = relationshipId };
+                var sheet1 = new Sheet() { Name = "Protected Sheet", SheetId = 1, Id = relationshipId };
                 sheets.Append(sheet1);
                 workbook.Append(sheets);
                 workbookPart.Workbook = workbook;
@@ -33,6 +27,7 @@ namespace WebApplication1.Controllers
                 var workSheetPart = workbookPart.AddNewPart<WorksheetPart>(relationshipId);
                 var workSheet = new Worksheet();
                 workSheet.Append(new SheetData());
+                workSheet.Append(new SheetProtection() { Sheet = true, Objects = true, Scenarios = true });
                 workSheetPart.Worksheet = workSheet;
 
                 //add document properties
@@ -40,31 +35,7 @@ namespace WebApplication1.Controllers
                 document.PackageProperties.ContentStatus = "Final";
                 document.PackageProperties.Created = DateTime.UtcNow;
 
-
-
-                CustomFilePropertiesPart customFilePropertiesPart1 = document.AddNewPart<CustomFilePropertiesPart>("rId4");
-                GenerateCustomFilePropertiesPart1Content(customFilePropertiesPart1);
-
             }
         }
-
-
-
-        private void GenerateCustomFilePropertiesPart1Content(CustomFilePropertiesPart customFilePropertiesPart1)
-        {
-            Op.Properties properties2 = new Op.Properties();
-            properties2.AddNamespaceDeclaration("vt", "http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes");
-
-            Op.CustomDocumentProperty customDocumentProperty1 = new Op.CustomDocumentProperty() { FormatId = "{D5CDD505-2E9C-101B-9397-08002B2CF9AE}", PropertyId = 2, Name = "_MarkAsFinal" };
-            Vt.VTBool vTBool1 = new Vt.VTBool();
-            vTBool1.Text = "true";
-
-            customDocumentProperty1.Append(vTBool1);
-
-            properties2.Append(customDocumentProperty1);
-
-            customFilePropertiesPart1.Properties = properties2;
-        }
-
     }
 }
